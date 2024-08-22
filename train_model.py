76cf12761b6d6172b8fc588e0889dd10017158b5
egre215 <104238208+egre215@users.noreply.github.com>
2024-08-22 14:09:52 -0600

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -12,7 +12,7 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = r"/home/miricow/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+project_path = os.getcwd("")
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path)
